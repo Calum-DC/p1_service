07399e03723347540a02c1a3c281cbf827338702
Calum-DC <calumcourtney12gmail.com>
2025-02-26 09:05:18 +0000

    tests

--- tests/test_1.py
+++ tests/test_1.py
@@ -2,6 +2,19 @@ import json
 import pytest
 import os
 
+
+@pytest.fixture(scope='function')
+def queue_url_mock(sqs_client):
+    '''create mock queues for use in the p1,2, and 3 tests'''
+    queue_urls = {
+        '1': sqs_client.create_queue(QueueName='P1Queue')['QueueUrl'],
+        '2': sqs_client.create_queue(QueueName='P2Queue')['QueueUrl'],
+        '3': sqs_client.create_queue(QueueName='P3Queue')['QueueUrl']
+    }
+    return queue_urls
+
+
+
 def test_health_check(client):
     """Test to ensure the /health route is working."""
     response = client.get('/health')
