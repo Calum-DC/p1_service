6f9b5324a69f2b9b7b64166429939a4d5878e2e3
Calum-DC <calumcourtney12gmail.com>
2025-03-12 15:54:12 +0000

    k8

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import threading
 app = Flask(__name__)
 
 # Load environment variables
-load_dotenv()
+
 sqs_client = boto3.client('sqs', region_name=os.getenv('AWS_REGION'))
 QUEUE_URL = os.getenv('SQS_P1_URL')
 TEAMS_WEBHOOK_URL = os.getenv('TEAMS_WEBHOOK')
